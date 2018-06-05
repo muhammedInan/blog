b73c843ff7319d82fd126c7e59b7787d2250a238
inan <muhammed-inan@outlook.com>
2018-06-05 19:22:49 +0200

    update diagrams and add file for mvc with sign

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 require_once 'vendor/autoload.php';
 
 use Controllers\Controller;
-
+/* get c = le controller , get t = fonction */
 if (isset($_GET['c']) && isset($_GET['t'])) {
     $class = 'Controllers\\'.ucfirst($_GET['c']).'Controller';
     $target = $_GET['t'];
@@ -22,7 +22,23 @@ if (isset($_GET['c']) && isset($_GET['t'])) {
         }
     }
 
+
     $controller = new Controller();
    // $controller->twig->render('error/404.html.twig');
     echo '404';
+}
+else
+{
+    $class = 'Controllers\\DefaultController';
+    $target = 'home';
+
+
+
+    if (class_exists($class, true)) {
+        $class = new $class();
+        if (in_array($target, get_class_methods($class))) {
+            call_user_func_array([$class, $target],array());
+            exit();
+        }
+    }
 }
